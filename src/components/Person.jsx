303c3518fd51fba4185f29e06e5fa18ded16c194
Steven Reales <stevenreales@gmail.com>
2022-10-14 22:02:02 -0500

    add picsum image

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
+
+const iconType = {
+    student: {
+        icon: "mortarboard",
+        text: "Estudiante"
+    },
+    teacher: {
+        icon: "briefcase",
+        text: "Profesor"
+    },
+    assistant: {
+        icon: "person",
+        text: "Asistente"
+    }
+}
+
 function Person({ data, handleClickDelete, handleClickEdit }) {
     const { dni, name, lastName, birth, type, email, user } = data
-    const iconType = {
-        student: "mortarboard",
-        teacher: "briefcase",
-        assistant: "person"
-    }
+    const [imageUrl, setImageUrl] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+    }, [])
+
+    useEffect(() => {
+        loading && getImageUrl()
+    }, [loading])
+    
+
+    const getImageUrl = async () => {
+        try {
+            const res = await fetch("https://picsum.photos/100/150")
+            setImageUrl(res.url)
+        } catch (error) {
+            console.error("Error al obtener imagen.", error)
+        }
+    } 
+
     return (
         <div className="card mb-3">
             <div className="row g-0">
                 <div className="col-md-4 d-flex justify-content-center align-items-center">
-                    <i className={`bi bi-${iconType[type]}`} style={{ fontSize: "50px" }}></i>
+                    <img src={imageUrl} alt="imagen de picsum" />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
@@ -18,6 +51,7 @@ function Person({ data, handleClickDelete, handleClickEdit }) {
                         <p className="card-text m-0 text-truncate"><i className="bi bi-balloon"></i> {birth}</p>
                         <p className="card-text m-0 text-truncate"><small><i className="bi bi-at"></i> {email}</small></p>
                         <p className="card-text m-0 text-truncate"><i className="bi bi-person-workspace"></i> {user}</p>
+                        <p className="card-text m-0 text-truncate"><i className={`bi bi-${iconType[type].icon}`}></i> {iconType[type].text}</p>
                     </div>
                 </div>
             </div>
